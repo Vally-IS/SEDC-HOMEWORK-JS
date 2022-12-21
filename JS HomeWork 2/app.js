2c2eac9b91ed6925bf9397acc9cf14147de3a27e
besar1611 <valisulejmani992@gmail.com>
2022-12-21 21:26:35 +0100

    Finished Home Woro #3

---
+++ JS HomeWork 2/app.js
@@ -0,0 +1,37 @@
+console.log ("Home Work 2  JS")
+
+let year = prompt("What is your birthday?")
+
+let yearBorn = (Number(year));
+
+let result = (yearBorn -4 ) %12;
+
+if  (isNaN(yearBorn)){
+    console.log("Please write a number");
+}else if (result===0){
+    console.log("Rat")
+}else if (result===1){
+    console.log("Ox")
+}else if (result===2){
+    console.log("Tiger")
+}else if (result===3){
+    console.log("Rabbit")
+}else if (result===4){
+    console.log("Dragon")
+}else if (result===5){
+    console.log("Snake")
+}else if (result===6){
+    console.log("Hourse")
+}else if (result===7){
+    console.log("Goat")
+}else if (result===9){
+    console.log("Monkey")
+}else if (result===9){
+    console.log("Rooster")
+}else if (result===10){
+    console.log("Dog")
+}else if (result===11){
+    console.log("Pig")
+}
+
+
